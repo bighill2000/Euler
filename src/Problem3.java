a6ef6c1cf6846eca4a7ee2872e42caa5bd762675
Mr. Glass <bighill2000@gmail.com>
2015-10-02 16:58:17 -0600

    Commit

--- src/Problem3.java
+++ src/Problem3.java
@@ -1,6 +1,40 @@
+import java.util.ArrayList;
+
 /**
  * Created by Mr. Glass on 10/2/2015.
  */
 public class Problem3 {
-    
+
+
+    public static void main(String[] args) {
+
+        ArrayList<Integer> primeFacts = new ArrayList<>();
+
+        int square = (int) Math.sqrt(600851475143L);
+        System.out.println(square);
+
+        for (int i = 1; i < square; i+=2) {
+//            System.out.println((square % i) + "  " + i);
+            if (CheckFactor(i,square)) {
+//                System.out.print("add");
+                primeFacts.add(i);
+            }
+
+        }
+System.out.println(primeFacts.get(primeFacts.size()));
+
+    }
+
+    private static boolean CheckFactor(int number, int square) {
+
+        if (number % 2 == 0) return false;
+        for (int i = 3; i < square; i++) {
+            if (number % i == 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
